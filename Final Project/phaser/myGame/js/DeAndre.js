3c82545335f8529e4840a81575535a1e87fe0095
kevinphilly <kply@ucsc.edu>
2018-05-10 22:09:56 -0700

    revamped dialogue system

--- Final Project/phaser/myGame/js/DeAndre.js
+++ Final Project/phaser/myGame/js/DeAndre.js
@@ -1,36 +1,21 @@
 //var CurrentTextID = Alarm;
+var interactText;
 
 function DeAndre(game, key, frame){
-	Phaser.Sprite.call(this, game, 0, 400, key, frame);
+	Phaser.Sprite.call(this, game, 0, 550, key, frame);
 
 	this.anchor.set(0.5);
 	
 	game.physics.enable(this);
 	this.body.collideWorldBounds = true;
+	this.enableBody = true;
 
 }
 
 DeAndre.prototype = Object.create(Phaser.Sprite.prototype);
 DeAndre.prototype.constructor = DeAndre;
 
-// DeAndre.prototype.startConversation = function(convoKey){
-// 	var speech = this.game.cache.getJSON('speech');
-	
-// 	this.game.paused = true;
-	
-// //	var text = game.add.text(100, 100, speech.CurrentTextID, {fill: '#facade'});
-// };
 
-// DeAndre.prototype.stopConversation = function(){
-// 	this.activeConversation = null;
-// 	this.activeConversationState = null;
-// 	this.game.paused = false;
-// };
-
-// DeAndre.prototype.updateConversationState = function(stateId){
-// 	this.activeConversationState = stateId;
-// 	$showConversationState(this.activeConversation, stateId);
-// };
 
 DeAndre.prototype.update = function(){
 	if(cursors.left.isDown ){
@@ -38,19 +23,33 @@ DeAndre.prototype.update = function(){
 	}else if(cursors.right.isDown ){
 		this.body.velocity.x = 150;
 				
-			
-	}else if(cursors.up.isDown){
-		this.body.velocity.y = -150;
-			
-	}else if(cursors.down.isDown){
-		this.body.velocity.y = 150;
 	}else{
 		this.body.velocity.x = 0;
 		this.body.velocity.y = 0;
 	}
 
+	if(game.physics.arcade.overlap(player, door)){
+		interactText = game.add.text(door.x, door.y - 50, 'E', {fill:"#facade"});
+		
+	}
+
+	if(game.physics.arcade.overlap(player, door) == false && interactText != null){
+		interactText.destroy();
+	}
+
+	if(game.input.keyboard.justPressed(Phaser.Keyboard.E) && game.physics.arcade.overlap(player, door)){
+		nextDialogue = 3;
+		currentText = dialogue[nextDialogue].Text;
+		currentDialogue(currentText);
+	}
+	
+
 	// if(game.input.keyboard.justPressed(Phaser.Keyboard.R)){
 	// 	this.startConversation("alarm");
 	// }
 			
+}
+
+function Interact(Interactable){
+	currentDialogue(currentText);
 }
--- Final Project/phaser/myGame/js/dialogue.js
+++ Final Project/phaser/myGame/js/dialogue.js
@@ -1,3 +1,53 @@
+var text;
+var button1;
+var button2;
+
+//Style the text in the dialogue boxes
+var style = {
+	fill: "#fff",
+	boundsAlignH: "center", 
+	boundsAlignV: "middle",
+	wordWrap: true,
+	wordWrapWidth: 600
+};
+
+//Function to print out the current dialogue
 function currentDialogue(key){
-	var text = game.add.text(100, 700, key, {fill: '#facade'});
+
+	//Destroys existing text
+	if(text != null){
+		text.destroy();
+	}
+	text = game.add.text(0, 0, key, style);
+	text.setTextBounds(0, 600, 800, 100);
+
+	if(dialogue[nextDialogue].Branch == true){
+		button1 = game.add.button(100, 700, 'button', choice1, this, 0);
+		text = game.add.text(100, 700, dialogue[nextDialogue + 1].id);
+		button2 = game.add.button(600, 700, 'button', choice2, this, 0);
+		text = game.add.text(600, 700, dialogue[nextDialogue + 2].id);
+	}else{
+		game.time.events.add(Phaser.Timer.SECOND * 5, destroyText, this);
+	}
 }
+
+function choice1(){
+	nextDialogue = nextDialogue + 1;
+	currentText = dialogue[nextDialogue].Text;
+	button1.destroy();
+	button2.destroy();
+	currentDialogue(currentText);
+}
+
+function choice2(){
+	nextDialogue = nextDialogue + 2;
+	currentText = dialogue[nextDialogue].Text;
+	button1.destroy();
+	button2.destroy();
+	currentDialogue(currentText);
+}
+
+function destroyText(){
+	text.destroy();
+}
+
